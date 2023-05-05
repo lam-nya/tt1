3385c31882933f51abad0c82700fd91f8f0c59a6
Igor K <lamnya@gmail.com>
2023-05-05 16:21:20 +0300

    Change context. Make binary.

--- hello.go
+++ hello.go
@@ -3,11 +3,14 @@ package main
 import "github.com/gin-gonic/gin"
 
 func main() {
+	greetings := "Hello"
 	router := gin.Default()
 	
 	router.GET("/", func(context *gin.Context) {
 //		context.HTML(200, "index.html", "")
-		context.File("index.html")
+//		context.File("index.html")
+		context.Writer.WriteHeader(200)
+		context.Writer.Write([]byte(greetings))
 	})
 
 	router.Run(":8181")
